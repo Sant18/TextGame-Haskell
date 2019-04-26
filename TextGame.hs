fc7e739805a40da5e7ca04ebce7b30ddc83f16ed
Sant18 <santachana@hotmail.co.uk>
2019-04-26 19:24:44 +0100

    Update TextGame.hs
    Added instructions

--- TextGame.hs
+++ TextGame.hs
@@ -34,4 +34,25 @@ locations =  [
     -- This is a hack, so I don't have to add more data to the "World" state
     ("spider", "alive")
     ]
+type World = (PathMap, LocationMap, Response)
+world :: IO (PathMap, LocationMap, Response)
+world = return (paths, locations, "")
 
+--Main function--
+main :: IO (String)
+main = do
+    putStrLn "\nThis is a Zombie game\n"
+    putStrLn instructions
+    play_game $ return (paths, locations, "")
+    return "Thank you for playing"
+    
+instructions =
+    "Enter commands using one or two words.\n" ++
+    "Available commands are:\n" ++
+    "main                                 -- to start the game.\n" ++
+    "north  south  east  west  up  down   -- to go in that direction.\n" ++"drop item                            -- to put down the item.\n" ++
+    "take item                            -- to pick up the item.\n" ++
+    "kill                                 -- to attack a zombie.\n" ++
+    "search                               -- to search the area.\n" ++
+    "supply                               -- to see your supplies.\n" ++
+    "quit game                            -- to end the game and quit."
